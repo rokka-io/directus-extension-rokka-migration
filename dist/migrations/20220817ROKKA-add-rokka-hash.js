fffb76f71c62ee23a7916cdd2627e79eb61bc624
Juerg Hunziker <juerg.hunziker@liip.ch>
2022-08-30 14:08:58 +0200

    check if rokka_hash already exists

--- dist/migrations/20220817ROKKA-add-rokka-hash.js
+++ dist/migrations/20220817ROKKA-add-rokka-hash.js
@@ -1,16 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.down = exports.up = void 0;
-const ROKKA_HASH_FIELD_NAME = 'rokka_hash';
+const ROKKA_HASH_COLUMN_NAME = 'rokka_hash';
 async function up(knex) {
-    await knex.schema.alterTable('directus_files', (table) => {
-        table.string(ROKKA_HASH_FIELD_NAME);
-    });
+    // Only create column if not already exists
+    if (!(await knex.schema.hasColumn('directus_files', ROKKA_HASH_COLUMN_NAME))) {
+        await knex.schema.alterTable('directus_files', (table) => {
+            table.string(ROKKA_HASH_COLUMN_NAME);
+        });
+    }
 }
 exports.up = up;
 async function down(knex) {
-    await knex.schema.alterTable('directus_files', (table) => {
-        table.dropColumn(ROKKA_HASH_FIELD_NAME);
-    });
+    // Only drop column if exists
+    if (await knex.schema.hasColumn('directus_files', ROKKA_HASH_COLUMN_NAME)) {
+        await knex.schema.alterTable('directus_files', (table) => {
+            table.dropColumn(ROKKA_HASH_COLUMN_NAME);
+        });
+    }
 }
 exports.down = down;
--- src/migrations/20220817ROKKA-add-rokka-hash.ts
+++ src/migrations/20220817ROKKA-add-rokka-hash.ts
@@ -1,15 +1,21 @@
 import { Knex } from 'knex';
 
-const ROKKA_HASH_FIELD_NAME = 'rokka_hash';
+const ROKKA_HASH_COLUMN_NAME = 'rokka_hash';
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_files', (table) => {
-		table.string(ROKKA_HASH_FIELD_NAME);
-	});
+	// Only create column if not already exists
+	if (!(await knex.schema.hasColumn('directus_files', ROKKA_HASH_COLUMN_NAME))) {
+		await knex.schema.alterTable('directus_files', (table) => {
+			table.string(ROKKA_HASH_COLUMN_NAME);
+		});
+	}
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_files', (table) => {
-		table.dropColumn(ROKKA_HASH_FIELD_NAME);
-	});
+	// Only drop column if exists
+	if (await knex.schema.hasColumn('directus_files', ROKKA_HASH_COLUMN_NAME)) {
+		await knex.schema.alterTable('directus_files', (table) => {
+			table.dropColumn(ROKKA_HASH_COLUMN_NAME);
+		});
+	}
 }
